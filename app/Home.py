ea9e63f5a7aa1f8d9352a8ebab02de13f1aac021
evelyncosta00 <148006346+evelyncosta00@users.noreply.github.com>
2024-08-04 14:39:51 -0300

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 st.sidebar.markdown('Desenvolvido por Evelyn Costa')
 
-st.markdown('# Bem-vindo ao Analisador de Vendas do seu Ecommerce')
+st.markdown('# Bem-vindo ao Analisador do seu Ecommerce')
 
 st.divider()
+
+st.image("https://raw.githubusercontent.com/evelyncosta00/analise_ecommerce/main/imagens/evino.jpg", use_column_width=True)
