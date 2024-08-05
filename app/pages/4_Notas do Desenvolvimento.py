c6b68c7fb3dced8404e60532b816ce8356561724
evelyncosta00 <148006346+evelyncosta00@users.noreply.github.com>
2024-08-05 01:41:13 -0300

    Update 4_Notas do Desenvolvimento.py

--- app/pages/4_Notas do Desenvolvimento.py
+++ app/pages/4_Notas do Desenvolvimento.py
@@ -7,26 +7,48 @@ import pandas as pd
 
 from utilidades import leitura_de_dados
 
-# Título do aplicativo
+
 st.markdown(''' # Notas do Desenvolvimento 	:pencil:''')
 
-# Divisor
+
 st.divider()
 
-# Descrição inicial
+
 st.markdown('''
 Primeiramente, importei as duas tabelas no Python para analisar as bases de dados. 
 Identifiquei as semelhanças e diferenças entre elas e segui a ordem indicada nas instruções do case para análise.
 Decidi explorar primeiro a base de dados Vendas_Evino.xlsx, realizei o tratamento dos nomes das colunas e a análise descritiva inicial.
 ''')
 
-# Carregar o CSV em um DataFrame
+
 url = "https://raw.githubusercontent.com/evelyncosta00/analise_ecommerce/main/bases/Vendas_Evino.csv"
 df = pd.read_csv(url, sep=';', encoding='latin1')
 
-# Remover colunas 'Unnamed'
+
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
-# Exibir as primeiras linhas do DataFrame
+
 df = df.head()
 st.table(df)
+
+st.markdown('''
+Analisando as informações contidas na base, decidi que seria melhor gerar um cubo com alguns agrupamentos, 
+como Região do Brasil para a coluna UF e faixa de idade para a coluna idade, facilitando a visualização e geração de insights. 
+Para gerar o cubo, decidi usar SQL para demonstrar minhas habilidades em queries e na integração do Python com bancos de dados, 
+algo crucial no ambiente corporativo onde frequentemente lidamos com dados provenientes de bancos de dados SQL.
+
+Criei uma variável engine para armazenar as informações de conexão e usei a função to_sql para escrever o DataFrame. 
+Em uma query SQL, criei as colunas de agrupamento por faixa etária (baseada na coluna idade) e por Região do Brasil 
+(baseada na coluna UF). Também criei as colunas soma_receita, soma_pedidos e soma_clientes, agrupando pelas cinco primeiras colunas. ''')
+
+url2 = "https://raw.githubusercontent.com/evelyncosta00/analise_ecommerce/main/bases/cubo_vendas.csv"
+df2 = pd.read_csv(url2, sep=';', encoding='latin1')
+
+
+df2 = df2.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+
+df2 = df2.head()
+st.table(df)
+
+
