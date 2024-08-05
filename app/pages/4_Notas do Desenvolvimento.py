e9b417b5d056bef28250f8049a086b3f2a34e3ca
evelyncosta00 <148006346+evelyncosta00@users.noreply.github.com>
2024-08-05 01:28:01 -0300

    Update 4_Notas do Desenvolvimento.py

--- app/pages/4_Notas do Desenvolvimento.py
+++ app/pages/4_Notas do Desenvolvimento.py
@@ -1,4 +1,4 @@
-from pathlib import c
+from pathlib import Path
 from datetime import date, timedelta
 
 import plotly.express as px
@@ -7,18 +7,19 @@ import pandas as pd
 
 from utilidades import leitura_de_dados
 
-
-
+# Título do aplicativo
 st.markdown(''' # Notas do Desenvolvimento 	:pencil:''')
 
-
+# Divisor
 st.divider()
 
-
-st.markdown('''Primeiramente, importei as duas tabelas no Python para analisar as bases de dados. 
+# Descrição inicial
+st.markdown('''
+Primeiramente, importei as duas tabelas no Python para analisar as bases de dados. 
 Identifiquei as semelhanças e diferenças entre elas e segui a ordem indicada nas instruções do case para análise.
 Decidi explorar primeiro a base de dados Vendas_Evino.xlsx, realizei o tratamento dos nomes das colunas e a análise descritiva inicial.
- 
-  ''')
+''')
 
+
+# Exibir o DataFrame
 st.table("https://raw.githubusercontent.com/evelyncosta00/analise_ecommerce/main/bases/Vendas_Evino.csv")
